2e0e6cc1d5411f6afb10ce3cceb53fb15b1d0e10
Alessandro Silva <robleadsc.ls@gmail.com>
2026-01-22 19:06:55 +0000

    Modification of stg_stripe__pauments.sql Addition of singular test

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -2,8 +2,9 @@ select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
-    status,
+    status as payment_stauts,
     amount / 100 as amount,
-    created as created_at
+    created as created_at,
+    _batched_at
     
 from {{ source('stripe', 'payment') }} 
