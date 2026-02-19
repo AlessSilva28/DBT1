1fdfcdc190f44c3178db57650396e337fb4c0b5a
Alessandro Silva <robleadsc.ls@gmail.com>
2026-02-19 20:57:02 +0000

    changes in : - staging models - customers_orders

--- models/marts/finance/fct_customer_orders.sql
+++ models/marts/finance/fct_customer_orders.sql
@@ -1,111 +1,105 @@
 with
 
-    raw_customers as (
-        select * from {{ source("jaffle_shop", "customers") }}
-    ),
+-- Import CTEs
 
-    raw_orders as (
-        select * from {{ source("jaffle_shop", "orders") }}
-    ),
+customers as (
 
-    raw_payments as ( 
-        select * from {{ source("stripe", "payment") }}
-    ),
+  select * from {{ ref('stg_jaffle_shop__customers') }}
 
-    payments as (
-        select
-            order_id,
-            created_at,
-            amount,
-            payment_status
-        from {{ ref('stg_stripe__payments') }}
-    ),
-    
-    orders as (
-        select
-            order_id,
-            customer_id,
-            order_date as order_placed_at,
-            order_status
-        from {{ ref('stg_jaffle_shop__orders') }}
-    ),
-     
-    customers as (
-        select  
-            customer_id,
-            first_name as customer_first_name,
-            last_name as customer_last_name
-        from {{ ref('stg_jaffle_shop__customers') }}
-    ),
+),
 
-    -- Logical CTEs
-    order_amount as (
-        select
-            order_id,
-            max(created_at) as payment_finalized_date,
-            sum(amount) as total_amount_paid
-        from payments
-        where payment_status <> 'fail'
-        group by 1
-    ),
+orders as (
 
-    paid_orders as (
-        select
-            o.order_id,
-            o.customer_id,
-            o.order_placed_at,
-            o.order_status,
-            p.total_amount_paid,
-            p.payment_finalized_date,
-            c.customer_first_name,
-            c.customer_last_name
-        from orders as o
-        left join order_amount as p on o.order_id = p.order_id
-        left join customers as c on o.customer_id = c.customer_id
-    ),
+  select * from {{ ref('stg_jaffle_shop__orders') }}
 
-    customer_orders as (
-        select
-            c.customer_id,
-            min(o.order_placed_at) as first_order_date,
-            max(o.order_placed_at) as most_recent_order_date,
-            count(o.order_id) as number_of_orders
-        from customers as c
-        left join orders as o on o.customer_id = c.customer_id
-        group by 1
-    ),
+),
 
-    orders_clv_bad as (
-        select 
-            p.order_id, 
-            sum(t2.total_amount_paid) as clv_bad
-        from paid_orders p
-        left join
-            paid_orders t2
-            on p.customer_id = t2.customer_id
-            and p.order_id >= t2.order_id
-        group by 1
-        order by p.order_id
-    ),
+payments as (
 
-    -- Final CTE
-    final as (
-        select
-            p.*,
-            row_number() over (order by p.order_id) as transaction_seq,
-            row_number() over (
-                partition by customer_id order by p.order_id
-            ) as customer_sales_seq,
-            case
-                when c.first_order_date = p.order_placed_at then 'new' else 'return'
-            end as nvsr,
-            x.clv_bad as customer_lifetime_value,
-            c.first_order_date as fdos
-        from paid_orders p
-        left join customer_orders as c using (customer_id)
-        left outer join orders_clv_bad as x on x.order_id = p.order_id
-        order by order_id
-    )
--- Simple Select Statment
-select *
-from final
+  select * from {{ ref('stg_stripe__payments') }}
+
+),
+
+-- Logical CTEs
+
+completed_payments as (
+
+  select 
+    order_id,
+    max(payment_created_at) as payment_finalized_date,
+    sum(payment_amount) as total_amount_paid
+  from payments
+  where payment_status <> 'fail'
+  group by 1
+
+),
+
+paid_orders as (
+
+  select 
+    orders.order_id,
+    orders.customer_id,
+    orders.order_placed_at,
+    orders.order_status,
+
+    completed_payments.total_amount_paid,
+    completed_payments.payment_finalized_date,
+
+    customers.customer_first_name,
+    customers.customer_last_name
+  from orders
+  left join completed_payments on orders.order_id = completed_payments.order_id
+  left join customers on orders.customer_id = customers.customer_id
+
+),
+
+-- Final CTE
+
+final as (
+
+  select
+    order_id,
+    customer_id,
+    order_placed_at,
+    order_status,
+    total_amount_paid,
+    payment_finalized_date,
+    customer_first_name,
+    customer_last_name,
+
+    -- sales transaction sequence
+    row_number() over (order by order_id) as transaction_seq,
+
+    -- customer sales sequence
+    row_number() over (partition by customer_id order by order_id) as customer_sales_seq,
+
+    -- new vs returning customer
+    case  
+      when (
+      rank() over (
+      partition by customer_id
+      order by order_placed_at, order_id
+      ) = 1
+    ) then 'new'
+    else 'return' end as nvsr,
+
+    -- customer lifetime value
+    sum(total_amount_paid) over (
+      partition by customer_id
+      order by order_placed_at
+      ) as customer_lifetime_value,
+
+    -- first day of sale
+    first_value(order_placed_at) over (
+      partition by customer_id
+      order by order_placed_at
+      ) as fdos
+
+    from paid_orders
+        
+)
+
+-- Simple Select Statement
+
+select * from final
+order by order_id
